a202b22ef1f5cff3dfd64a3a4ea5b1f444180143
tlecoz <fanthomas.lecoz@gmail.com>
2023-08-17 17:23:09 +0200

    add a script updateDoc.js , runable with npm run doc, used to generate the doc in ../xgpu-documentation and push the updated documentation on github (and deployed on netlify)

--- updateDoc.js
+++ updateDoc.js
@@ -1,38 +1,27 @@
 import { execSync } from 'child_process';
 
+//update package.json and define 'main' entry to 'dist/index.ts' 
 execSync('node set_xgpu_prod.js', { stdio: 'inherit' });
+
+//build xgpu and update 'dist'
 execSync('vite build && tsc --project tsconfig_build.json', { stdio: 'inherit' });
-// Générez la documentation
+
+//generate the documentation in ../xgpu-documentation/src
 execSync('npx tsjsondoc ./src ../xgpu-documentation/src/', { stdio: 'inherit' });
+
+//update back package.json and set 'main' to 'src/index.ts' so I can use locally synchronously with xgpu-samples 
 execSync('node set_xgpu_dev.js', { stdio: 'inherit' });
 
 try {
-    console.log("Changement de répertoire...");
+    console.log("Trying to push...");
     execSync('cd ../xgpu-documentation && git add . && git commit -m "update documentation" && git push', { stdio: 'inherit' });
 
-    console.log("Répertoire changé avec succès.");
+    console.log("documentation correctly updated on github & netlify");
 } catch (error) {
-    console.error('Erreur lors du changement de répertoire:', error);
-
-    try {
-        // Obtenez le nom de la branche actuelle
-        const branchName = execSync("cd ../xgpu-documentation && git rev-parse --abbrev-ref HEAD", { encoding: 'utf8' }).trim();
-        // Revenez à l'état du dernier push réussi pour cette branche
-        execSync(`cd ../xgpu-documentation && git fetch origin && git reset --hard origin/${branchName}`, { stdio: 'inherit' });
-    } catch (resetError) {
-        console.error('Erreur lors de la réinitialisation à l\'état du dernier push:', resetError);
-    }
-
+    console.error('Error during github push', error);
+    //get current branch name
+    const branchName = execSync("cd ../xgpu-documentation && git rev-parse --abbrev-ref HEAD", { encoding: 'utf8' }).trim();
+    //go back to the state of xgpu-documentation at the last successfull push 
+    execSync(`cd ../xgpu-documentation && git fetch origin && git reset --hard origin/${branchName}`, { stdio: 'inherit' });
 }
 
-
-/*
-// Exécutez les commandes git dans le répertoire xgpu-documentation
-execSync(`
-cd ../xgpu-documentation &&
-git add . &&
-git commit -m "update documentation" &&
-git push
-`, { stdio: 'inherit' });
-
-*/
